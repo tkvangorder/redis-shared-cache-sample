dd336dc343caa5f90e67e4e0c2574f0c7ad411ab
Tyler Van Gorder <tyler.vangorder@build.com>
2018-05-15 23:30:40 -0700

    Renaming application

--- examplev2/src/main/java/com/example/customer/CustomerApplication.java
+++ examplev2/src/main/java/com/example/customer/CustomerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProducerApplication {
+public class CustomerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
+		SpringApplication.run(CustomerApplication.class, args);
 	}
 }

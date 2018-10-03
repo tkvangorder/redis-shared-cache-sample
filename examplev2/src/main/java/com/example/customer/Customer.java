16685d3c2a24a72a4f6466d9712f2764a42b947a
Tyler Van Gorder <tyler.vangorder@build.com>
2018-10-03 08:10:16 -0700

    Fixing serialized UID

--- examplev2/src/main/java/com/example/customer/Customer.java
+++ examplev2/src/main/java/com/example/customer/Customer.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Customer implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
 
 	Long customerId;
 	String email;
